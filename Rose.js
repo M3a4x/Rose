5b45ab64491fe9d56ce78a8ae599b4e86d7c3bc0
M3a4x <38668201+M3a4x@users.noreply.github.com>
2018-07-16 15:50:41 +0300

    Update Rose.js

--- Rose.js
+++ Rose.js
@@ -7,7 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = 'f';
+const prefix = '*';
 const discord_token = (process.env.BOT_TOKEN);
 client.login(discord_token);
 client.on('ready', function() {
@@ -270,6 +270,6 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : M3a4x ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` Rose | #play  `,"http://twitch.tv/mjrmqm")
+client.user.setGame(` Rose | *play  `,"http://twitch.tv/mjrmqm")
 client.user.setStatus("dnd")
 });
