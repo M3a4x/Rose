87b71f28a1c955608d804e656ee058390dc5fbf2
M3a4x <38668201+M3a4x@users.noreply.github.com>
2018-07-22 23:50:04 +0300

    Update Rose.js

--- Rose.js
+++ Rose.js
@@ -227,6 +227,31 @@ function search_video(query, cb) {
 function isYoutube(str) {
     return str.toLowerCase().indexOf('youtube.com') > -1;
 }
+ client.on('message', message => {
+     if (message.content === prefix +"help") {
+    const embed = new Discord.RichEmbed()
+     .setColor("RANDOM")
+     .addField(`Zyad,aLmutairi commands:
++about - shows info about the bot
++ping - checks the bot's latency
+  Music:
++play - shows the song that is currently playing
++play <title|URL|subcommand> - plays the provided song
++queue [pagenum] - shows the current queue
++تعال <title|URL|subcommand> - plays the provided song
++skip - votes to skip the current song
+  DJ:
++ok <title|URL|subcommand> - plays the provided song
++skip - skips the current song
++pause - pauses the current song
++skipt <position> - skips to the specified song
++stop - stops the current song and clears the queue
++vol [0-150] - sets or shows volume
+For additional help,  `)
+
+      message.channel.send({embed});
+     }
+    });
 	client.on('message', message => {
     if (message.content.startsWith("habout")) {
       message.channel.send({
@@ -254,22 +279,3 @@ function isYoutube(str) {
          message.channel.send({embed:embed});
                         }
                     });
-
-
-client.on('ready', () => {
-  client.user.setGame(` By M3a4x #4272 .`,'https://www.twitch.tv/mjrmqm');
-  console.log('---------------');
-  console.log('Rose Bot Is Online')
-  console.log('---------------')
-});
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Rose Bot - Script By : M3a4x`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : M3a4x ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` 1play -  `,"http://twitch.tv/mjrmqm")
-client.user.setStatus("online")
-});
